3ee70b78374af4f9f960b62675ed78159058d200
zzzzz0001 <86714683+zzzzz0001@users.noreply.github.com>
2023-03-03 12:24:52 +0530

    Class library
    This can be used to generate dll by building it using .net

---
+++ Raw Code/Classes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Dll_Generator
+{
+    public class Class1
+    {
+        public void work()
+        {
+            Console.WriteLine("This is from Class1");
+        }
+    }
+
+    public class Class2
+    {
+        public void work()
+        {
+            Console.WriteLine("This is from Class2");
+        }
+    }
+
+    public class Class3
+    {
+        public void work()
+        {
+            Console.WriteLine("This is from Class3");
+        }
+    }
+
+    public struct Struct1
+    {
+        public int X { get; set; }
+        public float Y { get; set; }
+    }
+
+    public struct Struct2
+    {
+        public int X { get; set; }
+        public float Y { get; set; }
+    }
+}
